52dd7e8d4e113382b7a26a0d51d13be3d327e164
Sverre H. Huseby <shh@thathost.com>
2021-09-06 13:41:20 +0200

    Delete configs that are DYNAMIC_TOPIC_CONFIG, but no longer are set in the topology.

--- src/main/java/com/purbon/kafka/topology/actions/topics/builders/TopicConfigUpdatePlanBuilder.java
+++ src/main/java/com/purbon/kafka/topology/actions/topics/builders/TopicConfigUpdatePlanBuilder.java
@@ -51,8 +51,7 @@ public class TopicConfigUpdatePlanBuilder {
         .entries()
         .forEach(
             entry -> {
-              // TODO: This must check on other config sources as well - must be analyzed
-              if (!entry.isDefault() && !configKeys.contains(entry.name())) {
+              if (isDynamicTopicConfig(entry) && !configKeys.contains(entry.name())) {
                 topicConfigUpdatePlan.addConfigToDelete(entry.name(), entry.value());
               }
             });
