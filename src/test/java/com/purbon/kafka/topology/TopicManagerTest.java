410cdcd86137463bd7c9b802bf219ad8624f3850
Sverre H. Huseby <shh@thathost.com>
2021-09-07 11:28:46 +0200

    Add import that was lost during rebase

--- src/test/java/com/purbon/kafka/topology/TopicManagerTest.java
+++ src/test/java/com/purbon/kafka/topology/TopicManagerTest.java
@@ -21,6 +21,8 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import org.apache.kafka.clients.admin.Config;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
